934e7f65df1abee0dcb49a70bc5bc5b5a191f2fa
Jeff Greer <greergan@gmail.com>
2026-04-28 15:37:59 -0700

    Further code refinements to reduce number of computations

--- cmake/SlimFunctions.cmake
+++ cmake/SlimFunctions.cmake
@@ -87,14 +87,12 @@ endfunction()
 # _set_git_repo(<NAME>)  [internal]
 # ---------------------------------------------------------------------------
 function(_set_git_repo NAME)
+    meta_set(MODULE "${NAME}" git_repo  "${_SLIM_GIT_BASE}/${NAME}.git")
     meta_get(MODULE "${NAME}" git_repo _repo_url)
 
     find_program(_CURL_EXEC curl)
     if(NOT _CURL_EXEC)
-        message(WARNING "_set_git_repo: curl not found, skipping repo check for '${NAME}'")
-        meta_set(MODULE "${NAME}" git_repo_found "OFF")
-        _propagate_module("${NAME}")
-        return()
+        message(FATAL_ERROR "_set_git_repo: curl not found, '${NAME}'")
     endif()
 
     execute_process(
@@ -109,9 +107,11 @@ function(_set_git_repo NAME)
     if("${_http_code}" EQUAL 301)
         meta_set(MODULE "${NAME}" git_repo_found "ON")
     else()
-        message(WARNING "_set_git_repo: '${_repo_url}' returned ${_http_code}, expected 301")
-        meta_set(MODULE "${NAME}" git_repo_found "OFF")
+        message(FATAL_ERROR "_set_git_repo: '${_repo_url}' returned ${_http_code}, expected 301")
     endif()
+
+    _set_git_tag("${NAME}")  
+    _set_git_repo_latest_tag("${NAME}")
 
     _propagate_module("${NAME}")
 endfunction()
@@ -126,9 +126,12 @@ function(_set_git_tag NAME)
     if(SLIM_USE_LOCAL_SOURCE)
         if(_primary)
             meta_set(MODULE "${NAME}" git_tag  "0.0.0")
-            meta_set(MODULE "${NAME}" git_hash "none")
-            _propagate_module("${NAME}")
+            meta_set(MODULE "${NAME}" git_hash "local-src")
+        else()
+            meta_set(MODULE "${NAME}" git_tag  "0.0.0")
+            meta_set(MODULE "${NAME}" git_hash "pkg-config")
         endif()
+        _propagate_module("${NAME}")
         return()
     endif()
 
@@ -138,25 +141,26 @@ function(_set_git_tag NAME)
         COMMAND git ls-remote --tags --sort=-v:refname "${_repo_url}"
         OUTPUT_VARIABLE _tags_raw
         OUTPUT_STRIP_TRAILING_WHITESPACE
-        ERROR_QUIET
+        RESULT_VARIABLE _result
+        ERROR_VARIABLE  _error
     )
+    if(NOT _result EQUAL 0)
+        message(FATAL_ERROR "_set_get_git_tag: git ls-remote failed for '${_repo_url}'\n${_error}")
+    endif()
 
     string(REGEX MATCH "refs/tags/([^\^\\n]+)" _ "${_tags_raw}")
     set(_tag "${CMAKE_MATCH_1}")
-
-    if("${_tag}" STREQUAL "")
-        meta_set(MODULE "${NAME}" git_tag  "")
-        meta_set(MODULE "${NAME}" git_hash "")
-        _propagate_module("${NAME}")
-        return()
-    endif()
 
     execute_process(
         COMMAND git ls-remote "${_repo_url}" "refs/tags/${_tag}"
         OUTPUT_VARIABLE _hash_raw
         OUTPUT_STRIP_TRAILING_WHITESPACE
-        ERROR_QUIET
+        RESULT_VARIABLE _result
+        ERROR_VARIABLE  _error
     )
+    if(NOT _result EQUAL 0)
+        message(FATAL_ERROR "_set_get_git_tag: git ls-remote failed for '${_repo_url}' refs/tags/${_tag}\n${_error}")
+    endif()
 
     string(REGEX MATCH "^([a-f0-9]+)" _ "${_hash_raw}")
     set(_hash "${CMAKE_MATCH_1}")
@@ -173,27 +177,26 @@ endfunction()
 function(_set_git_repo_latest_tag NAME)
     meta_get(MODULE "${NAME}" git_repo_found _repo_found)
     if(NOT _repo_found)
-        meta_set(MODULE "${NAME}" git_latest_tag "")
-        _propagate_module("${NAME}")
-        return()
+        message(FATAL_ERROR "_set_git_repo_latest_tag: _repo_found not found, for '${NAME}'")
     endif()
 
     meta_get(MODULE "${NAME}" git_repo _repo_url)
 
     find_program(_GIT_EXEC git)
     if(NOT _GIT_EXEC)
-        message(WARNING "_set_git_repo_latest_tag: git not found, skipping tag fetch for '${NAME}'")
-        meta_set(MODULE "${NAME}" git_latest_tag "")
-        _propagate_module("${NAME}")
-        return()
+        message(FATAL_ERROR "_set_git_repo_latest_tag: git not found, skipping tag fetch for '${NAME}'")
     endif()
 
     execute_process(
         COMMAND "${_GIT_EXEC}" ls-remote --tags --sort=-version:refname "${_repo_url}"
         OUTPUT_VARIABLE _tag_output
         OUTPUT_STRIP_TRAILING_WHITESPACE
-        ERROR_QUIET
+        RESULT_VARIABLE _result
+        ERROR_VARIABLE  _error
     )
+    if(NOT _result EQUAL 0)
+        message(FATAL_ERROR "_set_get_git_repo_latest_tag: git ls-remote failed for '${_repo_url}'\n${_error}")
+    endif()
 
     string(REGEX MATCH "refs/tags/([^\n^]+)" _ "${_tag_output}")
     meta_set(MODULE "${NAME}" git_latest_tag "${CMAKE_MATCH_1}")
@@ -237,6 +240,15 @@ function(_set_check_module NAME MIN_VERSION MAX_VERSION)
     endforeach()
 
     meta_set(MODULE "${NAME}" found_version "${${NAME}_RESOLVED_VERSION}")
+    _propagate_module("${NAME}")
+endfunction()
+
+# ---------------------------------------------------------------------------
+# _set_metadata_file(<NAME>)  [internal]
+# ---------------------------------------------------------------------------
+function(_set_metadata_file NAME)
+    meta_get(MODULE "${NAME}" lower _lower)
+    meta_set(MODULE "${NAME}" metadata_file "${_lower}.pc")
     _propagate_module("${NAME}")
 endfunction()
 
@@ -287,9 +299,31 @@ function(_set_module_headers NAME)
 endfunction()
 
 # ---------------------------------------------------------------------------
-# define_module([NAME] [min_version] [max_version])
-#   No args: derives name from CMAKE_SOURCE_DIR and auto-loads required_packages.
+# _set_package_info(<NAME>)  [internal]
 # ---------------------------------------------------------------------------
+function(_set_package_info NAME)
+    string(TOUPPER "${NAME}" _upper)
+    string(TOLOWER "${NAME}" _lower)
+    meta_set(MODULE "${NAME}" upper "${_upper}")
+    meta_set(MODULE "${NAME}" lower "${_lower}")
+
+    if(ARGC GREATER 3 AND "${ARGV3}" STREQUAL "ON")
+        meta_set(MODULE "${NAME}" primary ON)
+    endif()
+
+    if(ARGC GREATER 1 AND NOT "${ARGV1}" STREQUAL "")
+        meta_set(MODULE "${NAME}" min_version   "${ARGV1}")
+    endif()
+    if(ARGC GREATER 2 AND NOT "${ARGV2}" STREQUAL "")
+        meta_set(MODULE "${NAME}" max_version   "${ARGV2}")
+    endif()
+    _propagate_module("${NAME}")
+endfunction()
+
+# -------------------------------------------------------------------------------
+# define_module([NAME] [min_version] [max_version] [ON])
+#   No args: derives name from CMAKE_SOURCE_DIR and auto-loads required_packages.
+# -------------------------------------------------------------------------------
 function(define_module)
     # ------------------------------------------------------------------
     # No-arg branch: primary module derived from CMAKE_SOURCE_DIR
@@ -336,34 +370,12 @@ function(define_module)
         return()
     endif()
 
-    # ------------------------------------------------------------------
-    # Named branch: compute and store all derived fields incrementally
-    # ------------------------------------------------------------------
-    set(NAME "${ARGV0}")
-
-#    set(_primary OFF)
-    if(ARGC GREATER 3 AND "${ARGV3}" STREQUAL "ON")
-        meta_set(MODULE "${NAME}" primary ON)
-    endif()
-
-    if(ARGC GREATER 1 AND NOT "${ARGV1}" STREQUAL "")
-        meta_set(MODULE "${NAME}" min_version   "${ARGV1}")
-    endif()
-    if(ARGC GREATER 2 AND NOT "${ARGV2}" STREQUAL "")
-        meta_set(MODULE "${NAME}" max_version   "${ARGV2}")
-    endif()
-
-    string(TOUPPER "${NAME}" _upper)
-    string(TOLOWER "${NAME}" _lower)
-    meta_set(MODULE "${NAME}" upper         "${_upper}")
-    meta_set(MODULE "${NAME}" lower         "${_lower}")
-    meta_set(MODULE "${NAME}" metadata_file "${_lower}.pc")
-    _set_module_headers("${NAME}")
-    meta_set(MODULE "${NAME}" git_repo      "${_SLIM_GIT_BASE}/${NAME}.git")
-    
-    _set_git_repo("${NAME}")
-    _set_git_tag("${NAME}")
-    _set_git_repo_latest_tag("${NAME}")    
-    
-    _propagate_module("${NAME}")
+    # ---------------------------------------------------------------------
+    # Re-entrant branch: compute and store all derived fields incrementally
+    # ---------------------------------------------------------------------
+    _set_package_info("${ARGV0}" ${ARGV1} ${ARGV2} ${ARGV3})
+    _set_metadata_file("${ARGV0}")
+    _set_module_headers("${ARGV0}")
+    _set_git_repo("${ARGV0}")
+    _propagate_module("${ARGV0}")
 endfunction()
